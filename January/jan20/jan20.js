3ef9d5fb4d8f1d973869662949b7f06d6a0ee2a5
Kumar Gourav <74108269+kgourav1@users.noreply.github.com>
2025-01-20 19:25:29 +0530

    Create jan20.js

---
+++ January/jan20/jan20.js
@@ -0,0 +1,42 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+function sortedMerge(head1, head2) {
+    // Determine the head of the merged list
+    let head;
+    if (head1.data < head2.data) {
+        head = head1;
+        head1 = head1.next;
+    } else {
+        head = head2;
+        head2 = head2.next;
+    }
+
+    // Pointer to track the current end of the merged list
+    let tail = head;
+
+    // Traverse both lists and merge nodes
+    while (head1 && head2) {
+        if (head1.data < head2.data) {
+            tail.next = head1;
+            head1 = head1.next;
+        } else {
+            tail.next = head2;
+            head2 = head2.next;
+        }
+        tail = tail.next;
+    }
+
+    // Append the remaining nodes of the non-empty list
+    if (head1) {
+        tail.next = head1;
+    } else {
+        tail.next = head2;
+    }
+
+    return head;
+}
